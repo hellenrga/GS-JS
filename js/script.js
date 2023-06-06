f3613278fcf99e72821dff944c06a8c0ee5105b7
hellenrga <hellenassis.dev@gmail.com>
2023-06-06 15:52:52 -0300

    aaSDASDASD

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+// Aguarde até que a página esteja completamente carregada
+document.addEventListener('DOMContentLoaded', function() {
+  // Obtenha o formulário de contato pelo seu ID
+  var form = document.getElementById('comunicacao');
+
+  // Adicione um ouvinte de evento de envio ao formulário
+  form.addEventListener('submit', function(event) {
+    // Evite que o formulário seja enviado imediatamente
+    event.preventDefault();
+
+    // Obtenha os valores dos campos de entrada
+    var tipoContato = document.getElementById('c-ajuda').value;
+    var nome = document.getElementById('c-nome').value;
+    var email = document.getElementById('c-email2').value;
+    var mensagem = document.getElementById('c-duvida').value;
+
+    // Faça algo com os valores dos campos
+    // Por exemplo, exiba-os no console
+    console.log('Tipo de Contato:', tipoContato);
+    console.log('Nome:', nome);
+    console.log('E-mail:', email);
+    console.log('Mensagem:', mensagem);
+
+    // Restaure o formulário para os valores padrão
+    form.reset();
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     var loginForm = document.forms.login;
   
@@ -20,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   
-    // Obtenha o formulário de notificação por e-mail
+
     var notifEmailForm = document.forms.notifEmail;
   
     notifEmailForm.addEventListener("submit", function(event) {
